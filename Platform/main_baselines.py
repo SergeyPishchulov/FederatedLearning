7ada75ed62f0cb22e49863f318545ec2a0784a4f
SergeyPishchulov <lcmrb26@bk.ru>
2023-12-01 10:11:25 +0300

    add copy args to fed task

--- Platform/main_baselines.py
+++ Platform/main_baselines.py
@@ -7,6 +7,7 @@ from utils import *
 from server_funct import *
 from client_funct import *
 import os
+from copy import deepcopy
 
 
 class FederatedMLTaskConfiguration:
@@ -16,9 +17,11 @@ class FederatedMLTaskConfiguration:
         self.epochs = epochs
         self.iid = 1  # data is iid
 
-#TODO подумать. Копировать args, заменять в них датасет, архитектуру и не париться
+
+# TODO подумать. Копировать args, заменять в них датасет, архитектуру и не париться
 class FederatedMLTask:
-    def __init__(self, node_cnt, conf: FederatedMLTaskConfiguration, random_seed):
+    def __init__(self, node_cnt, conf: FederatedMLTaskConfiguration, random_seed, args):
+        self.args = args
         self.data = Data(conf.dataset, node_cnt, 0, random_seed)  # TODO dumb
         self.conf = conf
         self.node_cnt = node_cnt
@@ -48,7 +51,7 @@ if __name__ == '__main__':
     fedeareted_tasks_configs.append(FederatedMLTaskConfiguration(
         dataset='cifar10',
         nn_architecture='ResNet20',
-        epochs='2'
+        epochs='2',
     ))
     fedeareted_tasks_configs.append(FederatedMLTaskConfiguration(
         dataset='cifar10',
@@ -58,33 +61,33 @@ if __name__ == '__main__':
     node_num = 5
     random_seed = 10
     conf = fedeareted_tasks_configs[0]
-    ft = FederatedMLTask(node_num, conf, random_seed)
+    ft = FederatedMLTask(node_num, conf, random_seed, args=deepcopy(args))
 
     # Start the FL training
     final_test_acc_recorder = RunningAverage()
     test_acc_recorder = []
     for rounds in range(args.T):
         print('===============Stage 1 The {:d}-th round==============='.format(rounds + 1))
-        lr_scheduler(rounds, ft.client_nodes, args)
+        lr_scheduler(rounds, ft.client_nodes, ft.args)
         # Client update
-        ft.client_nodes, train_loss = Client_update(args, ft.client_nodes, ft.central_node)
-        avg_client_acc = Client_validate(args, ft.client_nodes)
-        print(args.server_method + args.client_method + ', averaged clients personalization acc is ', avg_client_acc)
+        ft.client_nodes, train_loss = Client_update(ft.args, ft.client_nodes, ft.central_node)
+        avg_client_acc = Client_validate(ft.args, ft.client_nodes)
+        print(ft.args.server_method + ft.args.client_method + ', averaged clients personalization acc is ', avg_client_acc)
 
         # Partial select function
-        if args.select_ratio == 1.0:
+        if ft.args.select_ratio == 1.0:
             select_list = [idx for idx in range(len(ft.client_nodes))]
         else:
             select_list = generate_selectlist(ft.client_nodes, args.select_ratio)
 
         # Server update
-        ft.central_node = Server_update(args, ft.central_node, ft.client_nodes, select_list, ft.size_weights)
-        acc = validate(args, ft.central_node, which_dataset='local')
-        print(args.server_method + args.client_method + ', global model test acc is ', acc)
+        ft.central_node = Server_update(ft.args, ft.central_node, ft.client_nodes, select_list, ft.size_weights)
+        acc = validate(ft.args, ft.central_node, which_dataset='local')
+        print(ft.args.server_method + args.client_method + ', global model test acc is ', acc)
         test_acc_recorder.append(acc)
 
         # Final acc recorder
-        if rounds >= args.T - 10:
+        if rounds >= ft.args.T - 10:
             final_test_acc_recorder.update(acc)
 
-    print(args.server_method + args.client_method + ', final_testacc is ', final_test_acc_recorder.value())
+    print(ft.args.server_method + ft.args.client_method + ', final_testacc is ', final_test_acc_recorder.value())
