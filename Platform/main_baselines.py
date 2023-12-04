51f846704fc5a4cd06cfd934e74b91ef1918517a
SergeyPishchulov <lcmrb26@bk.ru>
2023-12-04 11:10:29 +0300

    fix

--- Platform/main_baselines.py
+++ Platform/main_baselines.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
         if next_ft_id is not None:
             ft = tasks[next_ft_id]
             Server_update(ft.args, ft.central_node.model, [n.model for n in ft.client_nodes],
-                          hub.get_select_list(ft),  # TODO note that local models are took from nodes, not from journal
+                          hub.get_select_list(ft, [c.id for c in clients]),  # TODO note that local models are took from nodes, not from journal
                           ft.size_weights)
             hub.journal.mark_as_aggregated(ft.id)
             for c in clients:  # TODO make through pipe
