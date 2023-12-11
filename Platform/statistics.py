5ae6b17dc71e536e03a0164f36eb3c09e7dc6cd3
SergeyPishchulov <lcmrb26@bk.ru>
2023-12-11 11:50:15 +0300

    fix

--- Platform/statistics.py
+++ Platform/statistics.py
@@ -38,7 +38,7 @@ class Statistics:
     def print_stat(self):
         res = timedelta(0)
         for ft_id, df in self.delay_by_ft_id.items():
-            print(df)
+            # print(df)
             res += df[self.client_cols].sum().sum()
             print(f'SUM_DELAY: {res}')
 
