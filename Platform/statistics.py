c6100c7d56c229e67bde9713d390e74b9af5e7ee
SergeyPishchulov <lcmrb26@bk.ru>
2023-12-25 12:45:20 +0300

    fix

--- Platform/statistics.py
+++ Platform/statistics.py
@@ -53,9 +53,9 @@ class Statistics:
             res += all_rounds_duration
         print(f"All rounds duration (sum by all tasks) {res.total_seconds()} s")
 
-    def save_client_ac(self, client_id, ft_id, round, acc, time_to_target_acc_sec):
-        self.acc_by_ft_id[ft_id].loc[round, f'client_{client_id}'] = acc
-        print(f"Saved acc for {f'client_{client_id}'} is {acc}")
+    def save_client_ac(self, client_id, ft_id, round_num, acc, time_to_target_acc_sec):
+        self.acc_by_ft_id[ft_id].loc[round_num, f'client_{client_id}'] = acc
+        print(f"Saved acc for {f'client_{client_id}'} is {round(acc, 3)}")
         if (time_to_target_acc_sec != -1) and (
                 self.time_to_target_acc.loc[ft_id, f'client_{client_id}'] is None):
             self.time_to_target_acc.loc[ft_id, f'client_{client_id}'] = time_to_target_acc_sec
