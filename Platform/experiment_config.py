47e17caf61abafc3da60ee7996f2bf77187896a6
SergeyPishchulov <lcmrb26@bk.ru>
2024-02-08 11:27:58 +0300

    fix

--- Platform/experiment_config.py
+++ Platform/experiment_config.py
@@ -5,24 +5,24 @@ def get_configs(user_args):
     common_config = dict(
         required_quality=1_000)
     custom_configs = []
-    # custom_configs.append(dict(
-    #     dataset='cifar10',
-    #     local_model='CNN',
-    #     target_acc=65,
-    #     interdeadline_time_sec=25
-    # ))
-    # custom_configs.append(dict(
-    #     dataset='cifar100',
-    #     local_model='ResNet20',
-    #     target_acc=65,
-    #     interdeadline_time_sec=25
-    # ))
     custom_configs.append(dict(
-        dataset='fmnist',
-        local_model='ResNet56',
-        target_acc=55,
-        interdeadline_time_sec=15
+        dataset='cifar10',
+        local_model='CNN',
+        target_acc=65,
+        interdeadline_time_sec=25
     ))
+    custom_configs.append(dict(
+        dataset='cifar100',
+        local_model='ResNet20',
+        target_acc=65,
+        interdeadline_time_sec=25
+    ))
+    # custom_configs.append(dict(
+    #     dataset='fmnist',
+    #     local_model='ResNet56',
+    #     target_acc=55,
+    #     interdeadline_time_sec=15
+    # ))
     return [Namespace(**(vars(user_args) | common_config | cc))
             for cc in custom_configs]
 
