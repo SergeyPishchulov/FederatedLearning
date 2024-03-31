ed4d4a056adeeffde9d02a423f9890e999377767
SergeyPishchulov <lcmrb26@bk.ru>
2024-03-31 16:47:10 +0300

    fix

--- Platform/hub.py
+++ Platform/hub.py
@@ -20,7 +20,7 @@ class Hub:
         self.write_q_by_cl_id, self.read_q_by_cl_id = self.init_qs()
         # self._init_scheduler(args)
         self.finished_by_client = {cl.id: False for cl in clients}
-        self.sent_jobs_ids: Set = {}
+        self.sent_jobs_ids: Set = set()
         # self.start_time: datetime.datetime = start_time
 
     # def _init_scheduler(self, args):
