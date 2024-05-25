d34e2bbd8773b73ce47021bb90253d8a056b5388
SergeyPishchulov <lcmrb26@bk.ru>
2024-05-25 19:01:18 +0500

    same tasks

--- Platform/config/experiment_config.py
+++ Platform/config/experiment_config.py
@@ -22,8 +22,9 @@ def get_configs(user_args):
     ))
     custom_configs.append(dict(
         dataset='cifar10',
-        local_model='ResNet56',
-        target_acc=55,
+        local_model='CNN',
+        # local_model='ResNet56',
+        target_acc=65,
         # interdeadline_time_sec=30
     ))
 #     custom_configs.append(dict(
